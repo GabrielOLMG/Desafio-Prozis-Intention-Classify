e332a2f68dec1cadc8b3fb7a7a52415ac9b4da21
gabrielluciano <gguimaraes@evolutio.pt>
2025-05-04 23:41:38 +0100

    Front end test

--- desafio_prozis/ml_models/views/home_view.py
+++ desafio_prozis/ml_models/views/home_view.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request, "home/home_page.html")
+    context = {
+        "username": request.user.username if request.user.is_authenticated else "",
+    }
+    return render(request, "home/home_page.html", context)
